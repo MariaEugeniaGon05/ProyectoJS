0540bb2c2ba28b9762a805ebc19f9be579876cbd
Maria Eugenia Goncalves <mariaeugeniagoncalves0502@gmail.com>
2023-05-29 12:56:17 -0300

    creacion pagina

---
+++ DomElements.js
@@ -0,0 +1,59 @@
+let vip = document.getElementById("vip");
+vip.innerHTML = `<div class="card">
+<img src="./imagenes/212VIP.jpg" class="card-img-top" alt="Producto 1">
+<div class="card-body">
+  <h5 class="card-title">212 Vip Carolina Herrera</h5>
+  <p class="card-text">descripcion</p>
+</div>
+</div>`;
+
+let amor = document.getElementById("amor");
+amor.innerHTML = `<div class="card">
+<img src="./imagenes/AmorAmor.jpg" class="card-img-top" alt="Producto 2">
+<div class="card-body">
+  <h5 class="card-title">Cacharel Amor Amor</h5>
+  <p class="card-text">Descripción del producto 2.</p>
+  <a href="#" class="btn btn-primary">Agregar al carro</a>
+</div>
+</div>`;
+
+let terra = document.getElementById("terra");
+terra.innerHTML = `<div class="card">
+<img src="./imagenes/AquaDiG.jpg" class="card-img-top" alt="Producto 3">
+<div class="card-body">
+  <h5 class="card-title">Armani Terra Di Gioia</h5>
+  <p class="card-text">Descripción del producto 3.</p>
+  <a href="#" class="btn btn-primary">Agregar al carro</a>
+</div>
+</div>`;
+
+let mywey = document.getElementById("mywey");
+mywey.innerHTML = `<div class="card">
+<img src="./imagenes/MyWey.jpg" class="card-img-top" alt="Producto 1">
+<div class="card-body">
+  <h5 class="card-title">My Wey Giorgio Armani</h5>
+  <p class="card-text">descripcion</p>
+  <a href="#" class="btn btn-primary">Agregar al carro</a>
+</div>
+</div>`;
+
+let si = document.getElementById("si");
+si.innerHTML = `<div class="card">
+<img src="./imagenes/Si.jpg" class="card-img-top" alt="Producto 1">
+<div class="card-body">
+  <h5 class="card-title">Armani Sí in-tense</h5>
+  <p class="card-text">descripcion</p>
+  <a href="#" class="btn btn-primary">Agregar al carro</a>
+</div>
+</div>`;
+
+let valentino = document.getElementById("valentino");
+valentino.innerHTML = `<div class="card">
+<img src="./imagenes/valentino.jpg" class="card-img-top cusatom-img" alt="Producto 1">
+<div class="card-body">
+  <h5 class="card-title">Valentino Born in Roma Yelow Donna</h5>
+  <p class="card-text">descripcion</p>
+  </div>
+</div>`;
+
+boton.addEventListener("click");
